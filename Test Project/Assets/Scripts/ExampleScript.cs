2d3b84e825f30e48453e339295c1d08607db80c4
Matthew Parker <maddparker@gmail.com>
2022-11-03 06:11:01 -0400

    Added test GameObject and Script

---
+++ Test Project/Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    public int var1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("var1: " + var1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
